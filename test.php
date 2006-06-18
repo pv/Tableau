cce37502bf11f0a4b6dd900861315259cb43a79f
Pauli Virtanen <pav@iki.fi>
2006-06-18 22:57:16 +0300

    A more sensible test case.

--- test.php
+++ test.php
@@ -10,43 +10,68 @@ $db_database = 'test';
 $connection = mysql_connect($db_host,$db_user,$db_password);
 mysql_select_db($db_database) or die("Unable to select database");
 
-$tableau = new PhpTableau($connection, 'fubar');
 
-$tableau->set_columns(
-    'id',       new IDColumn(),
-    'fubar',    new ChoiceColumn(array('a', 'b', 'c')),
-    'darkness', new DateTimeColumn(),
-    'saab',     new ForeignKeyColumn($connection, 'fubar', 'fubar', 'saab')
-    );
-$tableau->set_name(
-    'id', "ID",
-    'fubar', "FUBAR",
-    'darkness', "DARKNESS",
-    'saab', "Saab"
-    );
-$tableau->set_comment(
-    'id', "Identifier",
-    'fubar', "Fubar of foo",
-    'darkness', "FOO",
-    'saab', "FOO"
-    );
-
-function validate_fubar($value, &$msg) {
-    if ($value != "fubar") {
-        return true;
-    } else {
-        $msg = "Fubar cannot be \"fubar\"";
+function value_required($value, &$msg) {
+    if (!$value) {
+        $msg = "This field is required.";
         return false;
     }
+    return true;
 }
 
-function color_display($row, $field, &$disp, &$cell_attr) {
-    if ($field == 'saab' and !$row[$field]) {
-        $disp = "<a href=\"?action=edit&id=".$row['id']."\">(missing)</a>";
+if (false) {
+    $tableau = new PhpTableau($connection, 'staff');
+    
+    $tableau->set_columns(
+        'id',        new IDColumn(),
+        'name',      new TextColumn(),
+        'birthdate', new DateColumn(),
+        'phone',     new TextColumn()
+        );
+    $tableau->set_name(
+        'id', "ID",
+        'name', "Name",
+        'birthdate', "Birth date",
+        'phone', "Phone number"
+        );
+    $tableau->set_comment(
+        'id', "Identifier",
+        'name', "Name of the person",
+        'birthdate', "Birth date of the person",
+        'phone', "Work phone number extension"
+        );
+
+    $tableau->columns['birthdate']->set_range(date('Y') - 120,
+                                              date('Y') - 15);
+
+    function mark_missing($row, $field, &$disp, &$cell_attr) {
+        if ($field and !$row[$field]) {
+            $disp = "<a href=\"?action=edit&id=".$row['id']."\">(missing)</a>";
+        }
     }
+    $tableau->add_callback('display', mark_missing);
+    $tableau->add_validator('name', value_required);
+    $tableau->add_validator('birthdate', value_required);
+
+    $tableau->display();
+} else {
+    $tableau = new PhpTableau($connection, 'responsibilities');
+    $tableau->set_columns(
+        'id', new IDColumn(),
+        'name', new ForeignKeyColumn($connection,'staff','name','name'),
+        'responsibility', new ChoiceColumn(array('watering plants',
+                                                 'making fires',
+                                                 'calling fire brigade'))
+        );
+    $tableau->set_name(
+        'id', 'Identifier',
+        'name', 'Name',
+        'responsibility', 'Responsibility'
+        );
+    $tableau->set_comment(
+        'responsibility', 'What this guy or gal should do?');
+    $tableau->add_validator('name', value_required);
+    $tableau->add_validator('responsibility', value_required);
+
+    $tableau->display();
 }
-
-$tableau->add_validator('fubar', validate_fubar);
-$tableau->add_callback('display', color_display);
-
-$tableau->display();
